baf1f71096a3e754900fd5c42a91e168a2871140
sonesoul <smippy098@gmail.com>
2024-09-21 23:20:30 +0300

    Some changes in names.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Monoconsole")]
+[assembly: AssemblyTitle("monoconsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Monoconsole")]
+[assembly: AssemblyCompany("sonesoul")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
